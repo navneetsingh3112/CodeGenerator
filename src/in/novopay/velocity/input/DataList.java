e1688dae40fb9f3fd5c413b98f455afda6957b99
Abhishek Dey <abhishek.dey@novopay.in>
2017-09-11 12:21:14 +0530

    new file added

---
+++ src/in/novopay/velocity/input/DataList.java
@@ -0,0 +1,31 @@
+package in.novopay.velocity.input;
+
+public class DataList {
+    private String id;
+    private String code;
+    private String text;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
